17a07a03a50dd8a6f06f130486f38e840538a0b1
Shijin <shijin@aventusinformatics.com>
2024-02-02 11:19:36 +0530

    initial commit

---
+++ lib/util/app_config_constants.dart
@@ -0,0 +1,32 @@
+class AppConfigConstants {
+  // Name of app
+  static String appName = 'Selfyclub';
+  static String currentVersion = '1.7';
+
+  static String appTagline = 'Share your day activity with friends';
+  static const googleMapApiKey = "AIzaSyCTvBWb1e2I3pPzZ_oeFH4JzpKrjJ-pzgY";
+  
+  static const agoraApiKey="7ef3136ed2614657ab6c786462aac280";
+  
+  static const restApiBaseUrl = 'http://3.6.89.99/selfyclub/api/web/v1/';
+
+  // Socket api url
+  static const socketApiBaseUrl = "http://3.6.89.99:4000/";
+
+  // Socket api url
+  static const encryptionKey = "bbC4H19lsVbQDfakxc456rtNMQdd0FloL";
+
+  // chat version
+  static const int enableEncryption = 1;
+
+  // chat version
+  static const int chatVersion = 1;
+
+  // is demo app
+  static const bool isDemoApp = false;
+
+  // parameters for delete chat
+  static const secondsInADay = 86400;
+  static const secondsInThreeDays = 259200;
+  static const secondsInSevenDays = 604800;
+}
